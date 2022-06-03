dea56a2bfc10dd88594a42c93ecd8ce013cd1837
JohnnyNi <68974700+johnnyni2961@users.noreply.github.com>
2022-06-03 15:03:33 -0700

    Update Contact.jsx

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,5 +1,5 @@
 const Contact = () => {
-  return <h1>This is the Contact route. Should available to everybody.</h1>;
+  return <h1>Coming Soon... Stay tuned, thanks for being curious! Join our discord and try out app in the meantime!</h1>;
 };
 
 export default Contact;
