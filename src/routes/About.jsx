e57585433664f13e10b281a68c94ea5dc7f72538
JohnnyNi <68974700+johnnyni2961@users.noreply.github.com>
2022-06-03 15:03:46 -0700

    Update About.jsx

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,5 +1,5 @@
 const About = () => {
-  return <h1>This is the About route. Should available to everybody.</h1>;
+  return <h1>Coming Soon... Stay tuned, thanks for being curious! Join our discord and try out app in the meantime!</h1>;
 };
 
 export default About;
