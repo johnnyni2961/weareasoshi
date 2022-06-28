e0173511b3c2bed70b94e5270813224892aec9ee
JohnnyNi <68974700+johnnyni2961@users.noreply.github.com>
2022-06-27 23:52:29 -0700

    Update HeroMain.jsx

--- src/components/Hero/HeroMain.jsx
+++ src/components/Hero/HeroMain.jsx
@@ -10,8 +10,9 @@ const HeroMain = () => (
         <>
           Networking for{' '}
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
-            Students
+            Student 
           </span>
+          Founders
         </>
       }
     />
