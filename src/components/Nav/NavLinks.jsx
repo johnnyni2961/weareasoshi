b21f17b56e58f9ef9c8eb710b1f5e5c347f417a9
JohnnyNi <68974700+johnnyni2961@users.noreply.github.com>
2022-06-27 23:50:26 -0700

    Update NavLinks.jsx

--- src/components/Nav/NavLinks.jsx
+++ src/components/Nav/NavLinks.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const NavLinks = ({ showLogin }) => (
   <>
     <li>
-      <a src="https://asoshi.notion.site/ASOSHI-Memo-Public-Version-3b72486177da4b3cb48ca09a223c82ea">Learn More</a>
+      <a href="https://asoshi.notion.site/ASOSHI-Memo-Public-Version-3b72486177da4b3cb48ca09a223c82ea">Learn More</a>
     </li>
     <li className="lg:px-2" onClick={showLogin}>
       <a>Login</a>
