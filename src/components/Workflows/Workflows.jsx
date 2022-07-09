3c8f07bab17eec5426eeab386f593068b7ed5bdc
JohnnyNi <68974700+johnnyni2961@users.noreply.github.com>
2022-07-09 00:28:04 -0700

    Update Workflows.jsx

--- src/components/Workflows/Workflows.jsx
+++ src/components/Workflows/Workflows.jsx
@@ -12,13 +12,13 @@ const Workflows = () => (
       badge="Step 2"
       title="Launch/Grow Your Idea"
       description="Got a new idea that you're dying to pitch to other Gen-Z founders? Go to our explore page and reach the ENTIRE ASOSHI community. Got the ball rolling already? Reach out to driven Gen-Z folks ready to support your vision."
-      image="https://photos.app.goo.gl/wVNbWmui8CjHQWhB8"
+      image="https://i.ibb.co/jhY3XRp/two.png"
     />
     <WorkflowCard
       badge="Step 3"
       title="The Unknown Journey of Gen-Z"
       description="Ok. We'll be honest. We don't know what lies ahead for us, for Gen-Z. But we do know one thing, we pave our own paths. ASOSHI is built upon one core concept: community. Support each other. Innovate. ASOSHI isn't a 'company', we're a trend created by you. Your innovation defines us, we're just making the journey convenient."
-      image="https://photos.app.goo.gl/BFoxeo8Woj7r8qte8"
+      image="https://i.ibb.co/hRnwRd4/three.png"
     />
   </div>
 );
