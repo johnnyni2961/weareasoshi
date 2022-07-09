3c86ee3d7cb072924ac27a06942c5915b73b4991
JohnnyNi <68974700+johnnyni2961@users.noreply.github.com>
2022-07-09 00:26:06 -0700

    Update Workflows.jsx

--- src/components/Workflows/Workflows.jsx
+++ src/components/Workflows/Workflows.jsx
@@ -6,7 +6,7 @@ const Workflows = () => (
       badge="Step 1"
       title="Pitch yourself, dive into the ASOSHI community"
       description="'Rome wasn't built in a day.' Establish your brand. Are you driven to create the next neurodiversity project that receives a nobel prize or the next founder of a unicorn, VC-backed startup? It's your choice. Enter the app. Create profile. Be you."
-      image="https://photos.app.goo.gl/Hg4MCRzEHugnfbSf7"
+      image="https://i.ibb.co/vxLcW5W/one.png"
     />
     <WorkflowCard
       badge="Step 2"
