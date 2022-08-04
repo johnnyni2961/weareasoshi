905669d45bd655f96ce6d29cbb24f2890b3bc467
JohnnyNi <68974700+johnnyni2961@users.noreply.github.com>
2022-08-04 11:46:06 -0700

    Update NavLinks.jsx

--- src/components/Nav/NavLinks.jsx
+++ src/components/Nav/NavLinks.jsx
@@ -6,7 +6,9 @@ const NavLinks = ({ showLogin }) => (
       <a href="https://asoshi.notion.site/ASOSHI-Memo-Public-Version-3b72486177da4b3cb48ca09a223c82ea">Learn More</a>
     </li>
     <li>
-      <a href="paywall.html">Jump Ahead</a>
+      <a
+        to="/paywall"
+        href="/paywall">Dive Ahead</a>
     </li>
     <li className="lg:px-2" onClick={showLogin}>
       <a>Secret...</a>
