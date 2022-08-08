aa3c1a72410b4fefbb4af4fab973edf69ddcf66b
Johnny Ni <68974700+johnnyni2961@users.noreply.github.com>
2022-08-07 19:33:47 -0700

    Update HeroLinks.jsx

--- src/components/Hero/HeroLinks.jsx
+++ src/components/Hero/HeroLinks.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 const HeroLinks = () => (
   <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
     <div data-aos="fade-right" data-aos-delay="300">
-      <Link
-        to="/docs"
+      <a
         className="btn btn-primary w-full mb-4 sm:w-auto sm:mb-0"
-        href="/docs"
+        href="https://asoshi-paywall.vercel.app/"
+        target="_blank"
       >
         Get Started
-      </Link>
+      </a>
     </div>
     <div data-aos="fade-left" data-aos-delay="300">
       <a
